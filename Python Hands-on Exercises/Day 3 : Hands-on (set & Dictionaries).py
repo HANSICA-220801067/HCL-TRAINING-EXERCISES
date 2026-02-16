da51ba396e142fc5a339a6af2b1a4141bdd0b6be
HANSICA B S 220801067 <220801067@rajalakshmi.edu.in>
2026-02-16 19:05:36 +0530

    Update Day 3 : Hands-on (set & Dictionaries).py

--- Python Hands-on Exercises/Day 3 : Hands-on (set & Dictionaries).py
+++ Python Hands-on Exercises/Day 3 : Hands-on (set & Dictionaries).py
@@ -7,11 +7,39 @@ print(words)
 '''good morning  good night good evening
 ['evening', 'good', 'morning', 'night'] '''
 
-#1. Merge two inventory dicts by summing quantities of same keys.
+#2. Merge two inventory dicts by summing quantities of same keys.
 #Solution:
-text = (input().lower().split())
-words=sorted(set(text))
-print(words)
+dict1 =eval(input())
+dict2 =eval(input())
+for key in dict2:
+  if key in dict1:
+    dict1[key]=dict1[key]+dict2[key]
+  else:
+    dict1[key]=dict2[key]
+print(dict1)
 #Result:
-'''good morning  good night good evening
-['evening', 'good', 'morning', 'night'] '''
+'''{"apple":5,"banana":4,"grapes":3}
+{"apple":5,"banana":4,"tea":6}
+{'apple': 10, 'banana': 8, 'grapes': 3, 'tea': 6}'''
+
+#3. Given a dict and target value, return all keys with that value
+#Solution:
+d = eval(input())
+target = int(input())
+result = []
+for key in d:
+    if d[key] == target:
+        result.append(key)
+print(result)
+#RESULT:
+'''{"fig":3,"table":4}
+3
+['fig']'''
+
+#4. Remove duplicates (order NOT important)
+#Solution:
+lst = list(map(int, input().split()))
+print(list(set(lst)))
+#RESULT:
+'''1 2 2 2 3 5 3 6 4 6 
+[1, 2, 3, 4, 5, 6]'''
