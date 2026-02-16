33fff9fed458f6fb7032ef7f80267d20d00621fd
HANSICA B S 220801067 <220801067@rajalakshmi.edu.in>
2026-02-16 18:48:52 +0530

    Update Day 3 : Hands-on (set & Dictionaries).py

--- Python Hands-on Exercises/Day 3 : Hands-on (set & Dictionaries).py
+++ Python Hands-on Exercises/Day 3 : Hands-on (set & Dictionaries).py
@@ -1,1 +1,17 @@
+#1. Given text, return sorted unique words ignoring case.
+#Solution:
+text = (input().lower().split())
+words=sorted(set(text))
+print(words)
+#Result:
+'''good morning  good night good evening
+['evening', 'good', 'morning', 'night'] '''
 
+#1. Merge two inventory dicts by summing quantities of same keys.
+#Solution:
+text = (input().lower().split())
+words=sorted(set(text))
+print(words)
+#Result:
+'''good morning  good night good evening
+['evening', 'good', 'morning', 'night'] '''
